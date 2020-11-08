a4992128c4cf129c2a0bf1f8f88af1dc62e423dd
Scttpr <charles.capelli@qonfucius.team>
2020-11-08 18:41:16 +0100

    Feat - updated main.rs with dotenv and basic router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+
+mod router;
+mod api;
+
+use actix_web::{App, HttpServer};
+use dotenv;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().expect("Failed to parse .env file");
+
+    let host = dotenv::var("HOST").expect("HOST not found");
+    let port = dotenv::var("PORT").expect("PORT not found");
+    let url = format!("{}:{}", host, port);
+
+    HttpServer::new(||
+        App::new()
+            .service(router::config())
+    )
+        .bind(&url)?
+        .run()
+        .await
 }
+
+
+
+
